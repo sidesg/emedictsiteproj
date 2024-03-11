3e183f9f1785858fda0c3aa407ff08cf0f40adbb
G.S <sidesg@gmail.com>
2024-03-11 12:23:35 -0400

    More complete install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DIRECTORY=.venv
 
 if [ ! -d "$DIRECTORY" ]; then
@@ -6,10 +8,16 @@ else
     echo "$DIRECTORY exists"
 fi
 
-source .venv/bin/activate
+source $DIRECTORY/bin/activate
 pip install -r requirements.txt
 
+read -p "This script will delete the contents of emedict/migrations. Continue? (Y/N): " confirm 
+    && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
 cd emedictsite
+sudo rm -r emedict/migrations
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata emedictdata.json.gz --app emedict
+
+python manage.py createsuperuser
